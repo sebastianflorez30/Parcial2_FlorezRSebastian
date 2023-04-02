9276382e2af64243951b6535459d501973195d89
SEBASTIAN FLOREZ RAMIREZ <sebastianflorez303254@correo.itm.edu.co>
2023-04-02 06:58:31 -0500

    Add TryCast

--- Parcial2_FlorezRSebastian/Parcial2_FlorezRSebastian/Controllers/TicketsController.cs
+++ Parcial2_FlorezRSebastian/Parcial2_FlorezRSebastian/Controllers/TicketsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -12,71 +13,78 @@ namespace Parcial2_FlorezRSebastian.Controllers
 {
     public class TicketsController : Controller
     {
+        #region Constructor
         private readonly DatabaseContext _context;
 
         public TicketsController(DatabaseContext context)
         {
             _context = context;
         }
+        #endregion
 
-        // GET: Tickets
+        #region Private Methods
+        private async Task<Ticket> GetTicketById(Guid? Id)
+        {
+           
+            return await _context.Tickets.FirstOrDefaultAsync(t => t.Id == Id);              
+              
+        }
+
+        #endregion
+
+        #region Ticket Actions
         public async Task<IActionResult> Index()
         {
-              return _context.Tickets != null ? 
-                          View(await _context.Tickets.ToListAsync()) :
-                          Problem("Entity set 'DatabaseContext.Tickets'  is null.");
+            return View(await _context.Tickets
+                
+                .ToListAsync());
         }
 
-        // GET: Tickets/Details/5
-        public async Task<IActionResult> Details(Guid? id)
+        public IActionResult Create() //actionresult retornar cualquier tipo de cosa
         {
-            if (id == null || _context.Tickets == null)
-            {
-                return NotFound();
-            }
-
-            var ticket = await _context.Tickets
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (ticket == null)
-            {
-                return NotFound();
-            }
-
-            return View(ticket);
+            return View(); //retorna lo que se desee en view es una vista, en file un tipo de archivo
         }
 
-        // GET: Tickets/Create
-        public IActionResult Create()//actionresult retornar cualquier tipo de cosa
-        {
-            return View();//retorna lo que se desee en view es una vista, en file un tipo de archivo
-        }
-
-        // POST: Tickets/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Ticket ticket)
+        public async Task<IActionResult> Create(Ticket ticket)//task es una tarea que realiza una promesa; <tipo devuelve>
         {
             if (ModelState.IsValid)
             {
-                ticket.Id = Guid.NewGuid();
-                _context.Add(ticket);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                try
+                {
+                    ticket.UsedDate = DateTime.Now;
+                    _context.Add(ticket);
+                    await _context.SaveChangesAsync(); //savechangeasync es para hacer en base de datos el insert into, await permite continuar en segundo plano
+                    return RedirectToAction(nameof(Index)); //devuelve despues de realizar accion devuekver a la pestaña especifica de index
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe un ticket con el mismo numero.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
             }
             return View(ticket);
         }
 
-        // GET: Tickets/Edit/5
-        public async Task<IActionResult> Edit(Guid? id)
+        public async Task<IActionResult> Edit(Guid? Id)
         {
-            if (id == null || _context.Tickets == null)
+            if (Id == null || _context.Tickets == null)
             {
                 return NotFound();
             }
 
-            var ticket = await _context.Tickets.FindAsync(id);
+            var ticket= await GetTicketById(Id);
             if (ticket == null)
             {
                 return NotFound();
@@ -84,12 +92,9 @@ namespace Parcial2_FlorezRSebastian.Controllers
             return View(ticket);
         }
 
-        // POST: Tickets/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+        [HttpPost] //hacer una insercion en base de datos
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Guid id, Ticket ticket)
+        public async Task<IActionResult> Edit(Guid id, Ticket ticket) //task es una tarea que realiza una promesa; <tipo devuelve>
         {
             if (id != ticket.Id)
             {
@@ -100,35 +105,53 @@ namespace Parcial2_FlorezRSebastian.Controllers
             {
                 try
                 {
+                    ticket.UsedDate = DateTime.Now;
                     _context.Update(ticket);
-                    await _context.SaveChangesAsync();
+                    await _context.SaveChangesAsync(); //asyncronica es algo se ejecuta en segundo plano, sync es esperar a terminar esa accion. para consultas en base datos usa async await
+                    return RedirectToAction(nameof(Index));
                 }
-                catch (DbUpdateConcurrencyException)
+                catch (DbUpdateException dbUpdateException)
                 {
-                    if (!TicketExists(ticket.Id))
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
                     {
-                        return NotFound();
+                        ModelState.AddModelError(string.Empty, "Ya existe un tiquete con el mismo numero.");
                     }
                     else
                     {
-                        throw;
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
                     }
                 }
-                return RedirectToAction(nameof(Index));
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
             }
             return View(ticket);
         }
 
-        // GET: Tickets/Delete/5
-        public async Task<IActionResult> Delete(Guid? id)
+        public async Task<IActionResult> Details(Guid? Id)
         {
-            if (id == null || _context.Tickets == null)
+            if (Id == null || _context.Tickets == null) return NotFound();
+
+            var ticket = await _context.Tickets
+          
+                .FirstOrDefaultAsync(t => t.Id ==  Id);
+
+            if (ticket == null) return NotFound();
+
+            return View(ticket);
+        }
+
+        public async Task<IActionResult> Delete(Guid? Id)
+        {
+            if (Id == null || _context.Tickets == null)
             {
                 return NotFound();
             }
 
-            var ticket = await _context.Tickets
-                .FirstOrDefaultAsync(m => m.Id == id);
+            Ticket ticket = await _context.Tickets
+               
+                .FirstOrDefaultAsync(t => t.Id == Id);
             if (ticket == null)
             {
                 return NotFound();
@@ -137,28 +160,27 @@ namespace Parcial2_FlorezRSebastian.Controllers
             return View(ticket);
         }
 
-        // POST: Tickets/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(Guid id)
         {
             if (_context.Tickets == null)
             {
-                return Problem("Entity set 'DatabaseContext.Tickets'  is null.");
+                return Problem("Entity set 'DatabaseContext.Countries' is null.");
             }
-            var ticket = await _context.Tickets.FindAsync(id);
+            var ticket = await _context.Tickets
+               
+                .FirstOrDefaultAsync(t => t.Id == id);
+
             if (ticket != null)
             {
                 _context.Tickets.Remove(ticket);
             }
-            
+
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
-
-        private bool TicketExists(Guid id)
-        {
-          return (_context.Tickets?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
+#endregion
+        
     }
 }
