e2537b7e9346f244f1e61114b13630aecc51eaa2
SEBASTIAN FLOREZ RAMIREZ <sebastianflorez303254@correo.itm.edu.co>
2023-04-03 06:37:40 -0500

    Ultimate Modificate restriction

--- Parcial2_FlorezRSebastian/Parcial2_FlorezRSebastian/DAL/Entities/Ticket.cs
+++ Parcial2_FlorezRSebastian/Parcial2_FlorezRSebastian/DAL/Entities/Ticket.cs
@@ -6,8 +6,8 @@ namespace Parcial2_FlorezRSebastian.DAL.Entities
     public class Ticket : Entity
     {
         [Display(Name = "Codigo boleta")]        
-        [Range(0, short.MaxValue, ErrorMessage = "Debe ingresar un valor numérico.")]//solo recibe valores numericos
-        [RegularExpression("^.{0,5}$", ErrorMessage = "El campo {0} debe tener una longitud máxima de {1} caracteres.")] //Establece el tamaño que debe tener
+        //[Range(0, short.MaxValue, ErrorMessage = "Debe ingresar un valor numérico.")]//solo recibe valores numericos
+        [RegularExpression("^.{0,100000000000}$", ErrorMessage = "El campo {0} debe tener una longitud máxima de {1} caracteres.")] //Establece el tamaño que debe tener
         [Required(ErrorMessage = "El campo {0} es oblilgatorio.")] //errormessage permite enviar un mensaje de error
         public int CodTicket { get; set; }
 
